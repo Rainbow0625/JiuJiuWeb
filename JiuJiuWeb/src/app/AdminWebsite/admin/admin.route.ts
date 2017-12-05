0b1101a526951081ad212d0ae472a855a0efe671
Rainbow0625 <347601193@qq.com>
2017-12-05 11:07:08 +0800

    +

--- JiuJiuWeb/src/app/AdminWebsite/admin/admin.route.ts
+++ JiuJiuWeb/src/app/AdminWebsite/admin/admin.route.ts
@@ -6,20 +6,18 @@ import {GraperyComponent} from "./right/grapery/grapery.component";
 import {AdminRightComponent} from "./right/admin-right/admin-right.component";
 
 export const adminRoutes=[
-  {
-    path:'',
-    component:AdminComponent,
-    children:[{
-      // 默认显示的页面！怎么默认？
-      path:'adminHome',
-      component:AdminRightComponent,
-    },{
-      path:'typeTree',
-      component:TypeTreeComponent,
-    },{
-      path:'grapery',
-      component:GraperyComponent,
-    }]
+  {path:'', component:AdminComponent,
+    children:[
+      {
+        path:'',
+        component:AdminRightComponent,
+      },{
+        path:'typeTree',
+        component:TypeTreeComponent,
+      },{
+        path:'grapery',
+        component:GraperyComponent,
+      }]
   }
 ];
 
