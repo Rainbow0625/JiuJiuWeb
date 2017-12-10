df287868780ec2e91beea5084eb4c231ce7679a6
Rainbow0625 <347601193@qq.com>
2017-12-10 12:01:02 +0800

    +

--- JiuJiuWeb/src/app/app.module.ts
+++ JiuJiuWeb/src/app/app.module.ts
@@ -2,29 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
-import {HttpModule} from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import {CKEditorModule} from 'ng2-ckeditor';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-
-
-import { LoginComponent } from './HomePage/login/login.component';
-import { SignupComponent } from './HomePage/signup/signup.component';
-import { HomeComponent } from './HomePage/home/home.component';
-import { AdminComponent } from './AdminWebsite/admin/admin.component';
-import { UserComponent } from './UserWebsite/user/user.component';
 import {RouterModule, Routes} from '@angular/router';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { SidebarComponent } from './AdminWebsite/admin/sidebar/sidebar.component';
-import { AboutusComponent } from './HomePage/aboutus/aboutus.component';
-import { UsercenterComponent } from './UserWebsite/usercenter/usercenter.component';
-import { TypeTreeComponent } from './AdminWebsite/admin/right/type-tree/type-tree.component';
-=======
-import {ProductService} from './shared/product.service';
-import {RouterModule, Routes} from '@angular/router';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
->>>>>>> ae259a172b54fbd554daafb1d4f60efe0f13591c
 import {UserModule} from "./UserWebsite/user/user.module";
 import {AdminModule} from "./AdminWebsite/admin/admin.module";
 import {HomeModule} from "./HomePage/home/home.module";
@@ -38,17 +20,6 @@ import {ImageUploadModule} from "angular2-image-upload";
   declarations: [
     AppComponent,
     NotFoundPageComponent,
-    /*
-    AdminComponent,
-    UserComponent,
-    CarouselComponent,
-    SidebarComponent,
-
-    UsercenterComponent,
-    TypeTreeComponent,
-    BannerHeadComponent,
-    ProductComponent
-    */
   ],
   imports: [
     CKEditorModule,
@@ -61,11 +32,7 @@ import {ImageUploadModule} from "angular2-image-upload";
     UserModule,
     AdminModule,
     HomeModule,
-<<<<<<< HEAD
     HttpClientModule,
-=======
-    FormsModule,
->>>>>>> ae259a172b54fbd554daafb1d4f60efe0f13591c
     NgbModule.forRoot(),
     ImageUploadModule.forRoot(),
   ],
